779b0a5121aa440bd2a75fa74ac950b8c0665d02
A Matis <aleksamatis@yandex.ru>
2023-03-08 10:50:30 +0200

    timer improvement

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 import Timer from './components/Timer.vue'
 
 const timers = ref([])
@@ -7,8 +7,14 @@ const minutes = ref(0)
 const seconds = ref(0)
 
 function startTimer() {
-  timers.value.push({minutes: minutes.value, seconds: seconds.value})
+  if (minutes.value > 0 || seconds.value > 0) {
+    timers.value.push({minutes: minutes.value, seconds: seconds.value})
+  }
 }
+
+const canAddTimer = computed(() => {
+  return minutes.value > 0 || seconds.value > 0
+})
 </script>
 
 <template>
@@ -16,7 +22,7 @@ function startTimer() {
     <div>
       <input type="number" placeholder="minutes" v-model="minutes" min="0" max="60"/>
       <input type="number" placeholder="seconds" v-model="seconds" min="0" max="50" step="10"/>
-      <button @click="startTimer">Start timer</button>
+      <button @click="startTimer" :disabled="!canAddTimer">Start timer</button>
   </div>
     <Timer :totalMinutes="timer.minutes" :totalSeconds="timer.seconds" v-for="timer in timers"/>
   </main>
